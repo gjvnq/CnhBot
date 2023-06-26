26b7311ea4a80dbd447a3ed9cbdf806672ff13b9
Gabriela Noriega <gabrieljvnq@gmail.com>
2023-06-25 21:24:03 -0300

    ?

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿namespace com.github.gjvnq.cnhbot;
+﻿using dotenv.net;
+using Discord;
+using Discord.WebSocket;
+
+namespace com.github.gjvnq.cnhbot;
 
 class Program
 {
-    static void Main(string[] args)
+    private DiscordSocketClient _client = null!;
+
+    public static Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DotEnv.Load();
+        return new Program().MainAsync();
     }
+
+    public async Task MainAsync()
+	{
+        var envVars = DotEnv.Read();
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        await _client.LoginAsync(TokenType.Bot, envVars["CNHBOT__TOKEN"]);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+	}
+
+    private Task Log(LogMessage msg) {
+	Console.WriteLine(msg.ToString());
+	return Task.CompletedTask;
+}
 }
